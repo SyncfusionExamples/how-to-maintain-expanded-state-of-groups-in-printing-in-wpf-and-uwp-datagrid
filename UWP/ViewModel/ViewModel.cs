6f75632a4d12bf6193517d6a117072c85d75d72b
shobika.palani <shobika.palani@syncfusion.com>
2018-12-19 17:15:59 +0530

    How to maintain expanded state of groups in printing

---
+++ UWP/ViewModel/ViewModel.cs
@@ -0,0 +1,74 @@
+﻿using Syncfusion.UI.Xaml.Grid;
+using Syncfusion.UI.Xaml.Utility;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SfDataGridDemo
+{
+    public class CountryInfoViewModel : INotifyPropertyChanged, IDisposable
+    {
+        private BaseCommand printCommand;
+
+        public BaseCommand PrintCommand
+        {
+            get
+            {
+                if (printCommand == null)
+                    printCommand = new BaseCommand(OnPrintButtonClicked);
+                return printCommand;
+            }
+        }
+
+        private void OnPrintButtonClicked(object obj)
+        {
+            var dataGrid = obj as SfDataGrid;
+            if (dataGrid != null)
+            {
+                dataGrid.PrintSettings.PrintManagerBase = new CustomPrintManager(dataGrid);
+                dataGrid.PrintSettings.PrintManagerBase.Print();
+            }
+        }
+        public CountryInfoViewModel()
+        {
+            CountryDetails = new Countries();
+        }
+
+        private ObservableCollection<CountriesList> countryDetails;
+        public ObservableCollection<CountriesList> CountryDetails
+        {
+            get
+            {
+                return countryDetails;
+            }
+            set
+            {
+                countryDetails = value;
+                OnPropertyChanged("CountryDetails");
+            }
+        }
+
+        #region INotifyEventChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                handler(this, e);
+            }
+        }
+        #endregion
+
+        public void Dispose()
+        {
+            CountryDetails.Clear();
+        }
+    }
+}
---
+++ WPF/Helpers/CustomPrintManager.cs
@@ -0,0 +1,65 @@
+﻿#region Copyright Syncfusion Inc. 2001 - 2015
+// Copyright Syncfusion Inc. 2001 - 2015. All rights reserved.
+// Use of this code is subject to the terms of our license.
+// A copy of the current license can be obtained at any time by e-mailing
+// licensing@syncfusion.com. Any infringement will be prosecuted under
+// applicable laws. 
+#endregion
+using System.Data.Linq;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Media;
+using Syncfusion.UI.Xaml.Grid;
+using Syncfusion.Data;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SfDataGridDemo
+{
+    //CustomPrintManager Clas
+    internal class CustomPrintManager : GridPrintManager
+    {
+        //CustomPrintManager class constructor
+        public CustomPrintManager(SfDataGrid grid)
+               : base(grid)
+        {
+        }
+        protected override IList GetSourceListForPrinting()
+        {
+            if (View.GroupDescriptions.Any())
+            {
+                var records = ToIEnumerable(View.TopLevelGroup.GetEnumerator()).ToList();
+                List<NodeEntry> expandedGroups = new List<NodeEntry>();
+                foreach (var record in records)
+                {
+                    if (record is RecordEntry)
+                    {
+                        var group = record.Parent as Group;
+                        if (group != null && group.IsExpanded)
+                            expandedGroups.Add(record);
+                    }
+                    else if (record is Group)
+                    {
+                        var group = record as Group;
+                        if (group.IsTopLevelGroup || group.Level == 1)
+                        {
+                            expandedGroups.Add(group);
+                        }
+                        //For Multi-ColumnGrouping
+                        else
+                        {
+                            var parentGroup = group.Parent as Group;
+                            if (parentGroup != null && parentGroup.IsExpanded)
+                                expandedGroups.Add(group);
+                        }
+                    }
+                }
+                return expandedGroups;
+            }
+            return base.GetSourceListForPrinting();
+        }
+    }
+}
